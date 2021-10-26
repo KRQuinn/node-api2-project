5363100a60ca0e0d17ae268838b9ac7f6b241cf5
KRQuinn <kr.quinn82@gmail.com>
2021-10-26 16:14:33 -0700

    10/17 passing. GET and POST end points functional

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -37,16 +37,56 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// [GET] /api/posts/:id/comments - Returns an **array of all the comment objects** associated with the post with the specified id
-
 
 // [POST] /api/posts - Creates a post using the information sent inside the request body and returns **the newly created post object**
-
+router.post('/', (req, res) => {
+    const post = req.body
+    if (!post.title || !post.contents) {
+        res.status(400).json({
+            message: 'Please provide title and contents for the post',
+        })
+    } else {
+        Posts.insert(post)
+            .then(({ id }) => {
+            return Posts.findById(id)
+        })
+        .then((newPost) => {
+            res.status(201).json(newPost)
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({
+            message: 'There was an error while saving the post to the database',
+            })
+        })
+    }
+});
 
 // [PUT] /api/posts/:id - Updates the post with the specified id using data from the request body and **returns the modified document**, not the original
 
 
 // [DELETE] /api/posts/:id - Removes the post with the specified id and returns the **deleted post object**
+
+
+// [GET] /api/posts/:id/comments - Returns an **array of all the comment objects** associated with the post with the specified id
+
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const comments = await Posts.findPostComments(req.params.id)
+        if (!comments.length) {
+            res.status(404).json({ message: 'The post with the specified ID does not exist' })
+        } else {
+            res.status(200).json(comments)
+        }
+        
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'The comments information could not be retrieved',
+            rawError: err.message
+        })
+    }
+});
 
 
 module.exports = router
