203516ab8bb377bf9d3b6ec9a35ce6278d866438
KRQuinn <kr.quinn82@gmail.com>
2021-10-26 15:35:43 -0700

    3/17 passing. index and server set up, posts router connected and properly routing through one endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,37 @@
 // implement your posts router here
+const express = require('express');
+
+const router = express.Router();
+
+const Posts = require('./posts-model.js');
+
+// [GET] /api/posts - Returns **an array of all the post objects** contained in the database
+router.get('/', (req, res) => {
+    Posts.find(req.query)
+      .then(posts => {
+        res.status(200).json(posts);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: 'The posts information could not be retrieved',
+        });
+    });
+});
+
+// [GET] /api/posts/:id - Returns **the post object with the specified id**
+
+
+// [GET] /api/posts/:id/comments - Returns an **array of all the comment objects** associated with the post with the specified id
+
+
+// [POST] /api/posts - Creates a post using the information sent inside the request body and returns **the newly created post object**
+
+
+// [PUT] /api/posts/:id - Updates the post with the specified id using data from the request body and **returns the modified document**, not the original
+
+
+// [DELETE] /api/posts/:id - Removes the post with the specified id and returns the **deleted post object**
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,2 +1,23 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
+
+const server = express();
+
+server.use(express.json());
+
+// routers
+server.use('/api/posts', postsRouter);
+
+
+// Catch-all route
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Middle Earth Public Forum</h>
+      <p>Moderators online: Radaghast, Tom_Bombadil </p>
+    `);
+  });
+  
+  module.exports = server;
